27bddb955d7efb30faae1a2e3c6764d22617c7da
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-13 08:21:03 -0400

    Added new dashboard files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
