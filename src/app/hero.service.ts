9232b4e8117fce2b140b411595111aa4f635e9fa
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-13 07:59:07 -0400

    Completed the message service functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 
 // Importing the heroes and HEROES
@@ -12,11 +13,12 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  // New getHeroes using Obeservable
+  
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
