b2734fd5150e25df43f646eea02a2c7c456eb541
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2021-10-13 07:37:24 -0400

    Changed the getHeroes method to avoic compile errors

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,8 @@ export class HeroesComponent implements OnInit {
   
   // Added a getHeroes method here
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
